ee0d6dcbddbf869a537f7fff4499f33f33686865
Shashank Chauhan <shashank.chauhan.ec.2018@miet.ac.in>
2023-08-20 11:50:40 +0530

    Add project files.

---
+++ First Database/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace First_Database
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source = DESKTOP-IKIAV0Q\SQLEXPRESS;
+                                          Initial Catalog =  First Database;
+                                            User ID = pc;
+                                             Password=;
+                                            Trusted_Connection = Yes;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            Debug.WriteLine("Connected to the Server!");
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandType = CommandType.Text;
+            command.CommandText = "SELECT * FROM dbo.People";
+            SqlDataReader reader = command.ExecuteReader();
+            if(reader.HasRows)
+            {
+                while(reader.Read())
+                {
+                    Debug.WriteLine(reader.GetString(1)+ "-" + reader.GetString(2));
+                }
+            }
+            connection.Close();
+        }
+    }
+}
